43adf9bb07ec037dec97a9d497030c5f172b2dc0
Alex <garone80@gmail.com>
2023-07-23 08:27:05 +0300

    starter code

---
+++ NeuralNetwork.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Image.h"
+
+class NeuralNet
+{
+public:
+    using Matrix = std::vector<std::vector<float>>;
+
+public:
+    // Predict image label
+    uint8_t Predict(const Image& image) const;
+
+    // weight matrixes should use the row major (walking the matrix can be done row by row) representation
+    void SetWeights(std::vector<Matrix>&& weights);
+
+    void SetBiases(std::vector<std::vector<float>>&& biases);
+
+private:
+    std::vector<float> feedForward(const std::vector<uint8_t>& x) const;
+
+private:
+    std::vector<Matrix> m_weights;
+    std::vector<std::vector<float>> m_biases;
+};
